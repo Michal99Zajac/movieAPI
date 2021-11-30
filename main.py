02533b336097c14b902eeac58e653389c49d3cc4
WiktorWos <wiktor.wos99@gmail.com>
2021-11-30 13:30:45 +0100

    get actors movie with count endpoint

--- main.py
+++ main.py
@@ -90,18 +90,30 @@ def get_genres_by_ids(genre_ids):
 
 @app.get('/movies/all')
 def get_actor_movies(actor):
+    is_actor_found, titles = get_actor_titles(actor)
+    if is_actor_found:
+        return {"value": ", ".join(titles)}
+    return {}
+
+@app.get('/movies/')
+def get_actor_movies_count(actor):
+    is_actor_found, titles = get_actor_titles(actor)
+    if is_actor_found:
+        return {"value": titles[0], "count": len(titles)}
+    return {}
+
+def get_actor_titles(actor):
     actor_url = f"{API_URL}search/person{API_KEY}&query={actor}"
     actor_request_details = get_entity_id(actor_url)
     is_actor_found = actor_request_details[0]
+    titles = []
     if is_actor_found:
         actor_id = actor_request_details[1]
         movies_url = f"{API_URL}person/{actor_id}/movie_credits{API_KEY}"
         movies = requests.get(movies_url).json()["cast"]
-        titles = []
         for movie in movies:
             titles.append(movie["title"])
-        return {"value": ", ".join(titles)}
-    return {}
+    return is_actor_found, titles
 
 def get_entity_id(url):
     is_entity_found = False
